14586a18d5ee511eb1301a029d756a1984c819f7
Michael Kriese <michael.kriese@visualon.de>
2026-03-30 16:01:45 +0200

    ci: report coverage

--- jest.config.ts
+++ jest.config.ts
@@ -1,4 +1,7 @@
+import { env } from 'node:process';
 import type { JestConfigWithTsJest } from 'ts-jest/dist/types';
+
+const ci = !!env.CI;
 
 const config: JestConfigWithTsJest = {
   preset: 'ts-jest',
@@ -10,12 +13,19 @@ const config: JestConfigWithTsJest = {
   ],
   globals: {
     'ts-jest': {
-      diagnostics: Boolean(process.env.CI),
+      diagnostics: ci,
     },
   },
+  reporters: ci
+    ? [['github-actions', { silent: false }], 'summary']
+    : ['default'],
   verbose: true,
   collectCoverage: true,
   coveragePathIgnorePatterns: ['test-utils'],
+  coverageProvider: 'v8',
+  coverageReporters: ci
+    ? ['text-summary', 'json']
+    : ['text-summary', 'html', 'json'],
 };
 
 export default config;
